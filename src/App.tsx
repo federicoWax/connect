cf05d97fc94ae3590eda6ce271e9188aa776a086
federicoWax <cha_lino_23@hotmail.com>
2023-08-31 00:35:51 -0700

    ultimos cambios

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import AuthProvider from './context/AuthContext';
 import { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
+ /*  useEffect(() => {
     if(window.location.hostname.includes("connect-4dee9.web.app")) {
       window.location.replace('https://www.google.com.mx/')
     }
-  }, []);
+  }, []); */
   
   return (
     <AuthProvider>
